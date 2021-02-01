e25ed66c53cc814faa04f39b47341c76296b94cd
mystzd <grant@granthood.com>
2021-02-01 11:41:20 -0600

    yuh

--- commands/News/Politics/politics.js
+++ commands/News/Politics/politics.js
@@ -2,7 +2,8 @@ const Command = require("../../../base/Command.js");
 const { RichEmbed } = require('discord.js');
 const fetch = require('node-fetch');
 const Discord = require('discord.js');
-const keys = require('../../../rapidapikeys.js')
+const keys = require('../../../rapidapikeys.js');
+const request = require('request');
 
 class politicsSearch extends Command {
 	constructor(client) {
@@ -17,7 +18,78 @@ class politicsSearch extends Command {
 	}
 
 	async run(message, args) {
-		message.channel.send(args.join(" "));
+		const filter = m => message.author.id;
+		message.channel.send("Please enter a search term. Up to 10 characters. You have 10 seconds to complete this action.").then(() => {
+			message.channel.awaitMessages(filter, {
+				max: 1,
+				time: 10000,
+				errors: ['time']
+			})
+			.then(message => {
+				message = message.first();
+				const messageSearch = message;
+				const messageSearchTable = {
+					"word": `${messageSearch.content}`
+				}
+				message.channel.send(`You have chosen to search about \`${messageSearch.content}\`.`).then(() => {
+					message.channel.send("Please select the country you would like to search in. 2-char country codes only.").then(() => {
+						const filterTwo = m => message.author.id;
+						message.channel.awaitMessages(filterTwo, {
+							max: 1,
+							time: 10000,
+							errors: ['time']
+						})
+						.then(message => {
+							message = message.first()
+							const messageCountry = message;
+							const messageCountryTable = {
+								"word": `${messageCountry.content}`
+							}
+							message.channel.send(`You have chosen to search in \`${messageCountry.content}\``).then(() => {
+								message.channel.send("Please select the language of the article you would like to search. 2-char language codes only.").then(() => {
+									const filterThree = m => message.author.id;
+									message.channel.awaitMessages(filterThree, {
+										max: 1,
+										time: 10000,
+										errors: ['true']
+									})
+									.then(message => {
+										message = message.first();
+										const messageLang = message;
+										const messageLangTable = {
+											"word": `${messageLang.content}`
+										}
+										message.channel.send(`You have chosen to search in the \`${messageLang.content}\` language.`).then(() => {
+											message.channel.send(`Here are the articles of \`${messageSearchTable["word"]}\`, searching in the \`${messageCountryTable["word"]}\`, in the \`${messageLangTable["word"]}\` language.`).then(() => {
+												const options = {
+													method: 'GET',
+													url: 'https://newscatcher.p.rapidapi.com/v1/search',
+													qs: { q: `${messageSearchTable["word"]}`, country: `${messageCountryTable["word"]}`, lang: `${messageLangTable["word"]}`, topic: `politics`, sort_by: 'date', media: 'True' },
+													headers: {
+														"x-rapidapi-key": `${keys.key}`,
+														"x-rapidapi-host": `${keys.host}`
+													},
+													useQueryString: true
+												}
+												request(options, function(error, response, body) {
+													if(error) throw new Error(error);
+													console.log(body);
+												})
+											})
+											.catch(collected => {
+												message.channel.send('timeout');
+											})
+										
+										})
+									})
+								})
+							})
+						})
+					})
+				})
+			})
+			
+		})
 	}
 }
 
--- commands/Owner/eval.js
+++ commands/Owner/eval.js
@@ -1,40 +1,42 @@
 const Command = require("../../base/Command.js");
 const { inspect } = require("util");
 
-class Eval extends Command { 
-	constructor(client) {
-		super(client, {
-			name: "eval",
-			description: "Evaluates arbitrary javascript",
-			usage: "eval <code>",
-			ownerOnly: true,
-			category: "Owner", 
-			aliases: ["ev"],
-			extended: "Evaluates any javascript/nodejs code for bot owner only."
-		});
-	}
+class Eval extends Command {
+  constructor(client) {
+    super(client, {
+      name: "eval",
+      description: "Evalutes arbitrary javascript",
+      usage: "eval <code>",
+      ownerOnly: true,
+      category: "Owner",
+      aliases: ["ev"],
+      extended: "Evalutes any javascript/nodejs code for bot owner only."
+    });
+  }
+  
+  async run(message, args) {
+    const code = args.join(" ");
+    const token = this.client.token.split("").join("[^]{0,2}");
+    const rev = this.client.token.split("").reverse().join("[^]{0,2}");
+    const filter = new RegExp(`${token}|${rev}`, "g");
+    try {
+      let output = eval(code);
+      if (output instanceof Promise || (Boolean(output) && typeof output.then === "function" && typeof output.catch === "function")) output = await output;
+      output = inspect(output, { depth: 0, maxArrayLength: null });
+      output = output.replace(filter, "[TOKEN]");
+      output = clean(output);
+      message.channel.send(output, { code: "js", split: "\n" });
+      
+    } catch (error) {
+      message.channel.send(`The following error occured \`\`\`js\n${error}\`\`\``);
+    }
 
-	async run(message, args) {
-		const code = args.join(" ");
-		const token = this.token.split("").join("[^]{0,2}");
-		const rev = this.client.token.split("").reverse().join("[^]{0,2}");
-		const filter = new RegExp(`${token}|${rev}`, "g");
-		try {
-			let output = eval(code);
-			if(output instanceof Promise || (Boolean(output) && typeof output.then === "function" && typeof output.catch === "function")) output = await output;
-			output = inspect(output, { depth: 0, maxArrayLength: null });
-			output = output.replace(filter, "[TOKEN]");
-			output = clean(output);
-			message.channel.send(output, { code: "js", split: "\n" });
-		} catch (error) {
-			message.channel.send(`The following error occured \`\`\`js\n${error}\`\`\``);
-		}
-
-		function clean(text) {
-			return text
-			.replace(/`/g, "`" + String.fromCharCode(8203))
-			.replace(/@/g, "@" + String.fromCharCode(8203));
-		}
-	}
+    function clean(text)  {
+      return text
+        .replace(/`/g, "`" + String.fromCharCode(8203))
+        .replace(/@/g, "@" + String.fromCharCode(8203));
+    }
 }
+}
+
 module.exports = Eval;
